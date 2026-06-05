5d8be3c98a2d73a531e5d94140cb62a0ac7a93f1
MahmoudEmad114 <sci.MahmoudEmad77352@alexu.edu.eg>
2026-06-06 02:57:22 +0300

    update mongo connection

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ dotenv.config({ path: './.env' })
 const app = require('./app');
 
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-console.log(DB);
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// console.log(DB);
 
-mongoose
-    .connect(DB)
+// mongoose
+//     .connect(DB)
+//     .then(() => {
+//         console.log('DB connection successful!');
+//         console.log('Current DB Name:', mongoose.connection.name);
+//         console.log('Connection readyState:', mongoose.connection.readyState);
+//     });
+
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('DB connection successful!');
         console.log('Current DB Name:', mongoose.connection.name);
