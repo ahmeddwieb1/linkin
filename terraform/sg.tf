bd207b774a5064b57bbd9a20f5060a518bcb101f
ahmeddwieb1 <ahmeddwieb713@gmail.com>
2026-07-08 15:55:43 +0300

    add nginx.config and terraform v1

---
+++ terraform/sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "linkin_sg" {
+  name        = "linkin_sg"
+  vpc_id      = aws_vpc.mainVPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg.id] 
+  }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+    ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
